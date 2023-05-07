6cfd935d25dee8e9673bd641f207523394341adf
ktro2828 <kotaro.uetake@tier4.jp>
2023-05-07 11:15:37 +0900

    feat: add error type
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- src/evaluation_task.rs
+++ src/evaluation_task.rs
@@ -1,5 +1,16 @@
 use std::fmt::{Display, Formatter, Result as FormatResult};
 use std::str::FromStr;
+use thiserror::Error as ThisError;
+
+pub type EvaluationTaskResult<T> = Result<T, EvaluationTaskError>;
+
+#[derive(Debug, ThisError)]
+pub enum EvaluationTaskError {
+    #[error("internal error")]
+    InternalError,
+    #[error("value error")]
+    ValueError,
+}
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum EvaluationTask {
@@ -15,14 +26,14 @@ impl Display for EvaluationTask {
 }
 
 impl FromStr for EvaluationTask {
-    type Err = ();
+    type Err = EvaluationTaskError;
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
         match input {
             "Detection" | "detection" => Ok(EvaluationTask::Detection),
             "Tracking" | "tracking" => Ok(EvaluationTask::Tracking),
             "Prediction" | "prediction" => Ok(EvaluationTask::Prediction),
-            _ => Err(()),
+            _ => Err(EvaluationTaskError::ValueError),
         }
     }
 }
@@ -40,6 +51,6 @@ impl EvaluationTask {
     }
 }
 
-pub fn set_task(task_name: &str) -> Result<EvaluationTask, ()> {
+pub fn set_task(task_name: &str) -> EvaluationTaskResult<EvaluationTask> {
     EvaluationTask::from_str(task_name)
 }
