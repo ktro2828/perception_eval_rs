216e2bfd82005c657e0f54882ef97f3e4cc90f89
ktro2828 <kotaro.uetake@tier4.jp>
2023-05-11 12:00:24 +0900

    feat: update manager
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- src/manager.rs
+++ src/manager.rs
@@ -1,7 +1,10 @@
 use chrono::NaiveDateTime;
 
 use crate::{
-    config::PerceptionEvaluationConfig, dataset::FrameGroundTruth, object::object3d::DynamicObject,
+    config::PerceptionEvaluationConfig,
+    dataset::{get_current_frame, load_dataset, nuscenes::error::NuScenesResult, FrameGroundTruth},
+    filter::filter_objects,
+    object::object3d::DynamicObject,
 };
 
 pub trait EvaluationManager {
@@ -10,13 +13,14 @@ pub trait EvaluationManager {
         timestamp: NaiveDateTime,
         estimated_objects: Vec<DynamicObject>,
     ) -> T;
-    fn filter_objects(&self, estimated_objects: Vec<DynamicObject>) -> Vec<DynamicObject>;
-    fn get_frame_ground_truth(&self, timestamp: NaiveDateTime) -> FrameGroundTruth;
+    fn filter_objects(&self, objects: &Vec<DynamicObject>, is_gt: bool) -> Vec<DynamicObject>;
+    fn get_frame_ground_truth(&self, timestamp: NaiveDateTime) -> Option<FrameGroundTruth>;
 }
 
 #[derive(Debug, Clone)]
 pub struct PerceptionEvaluationManager {
     config: PerceptionEvaluationConfig,
+    frame_ground_truths: Vec<FrameGroundTruth>,
 }
 
 impl EvaluationManager for PerceptionEvaluationManager {
@@ -26,6 +30,21 @@ impl EvaluationManager for PerceptionEvaluationManager {
         estimated_objects: Vec<DynamicObject>,
     ) -> T {
     }
-    fn filter_objects(&self, estimated_objects: Vec<DynamicObject>) {}
-    fn get_frame_ground_truth(&self, timestamp: NaiveDateTime) {}
+    fn filter_objects(&self, objects: &Vec<DynamicObject>, is_gt: bool) -> Vec<DynamicObject> {
+        filter_objects(objects, is_gt, &self.config.filter_params)
+    }
+    fn get_frame_ground_truth(&self, timestamp: NaiveDateTime) -> Option<FrameGroundTruth> {
+        get_current_frame(&self.frame_ground_truths, &timestamp)
+    }
+}
+
+impl PerceptionEvaluationManager {
+    pub fn new(config: PerceptionEvaluationConfig) -> NuScenesResult<Self> {
+        let frame_ground_truths = load_dataset(version, data_root, evaluation_task, frame_id)?;
+        let ret = Self {
+            config: config,
+            frame_ground_truths: frame_ground_truths,
+        };
+        Ok(ret)
+    }
 }
