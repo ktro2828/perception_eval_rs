8bb146b497ae39fd332443e94de3fdf6e826b82c
ktro2828 <kotaro.uetake@tier4.jp>
2023-05-13 18:58:58 +0900

    feat: update detection metrics
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- src/metrics/detection.rs
+++ src/metrics/detection.rs
@@ -1,35 +1,66 @@
-use crate::{
-    label::Label, matching::MatchingMode, result::PerceptionResult, threshold::get_label_threshold,
-};
+use super::tp_metrics::{TPMetrics, TPMetricsAP, TPMetricsAPH};
+use crate::{label::Label, matching::MatchingMode, result::PerceptionResult};
+use std::collections::HashMap;
 
-use super::tp_metrics::TPMetrics;
+#[derive(Debug)]
+pub(crate) struct DetectionMetricsScore {
+    pub(crate) scores: HashMap<String, f64>,
+}
 
-// #[derive(Debug)]
-// pub(crate) struct DetectionMetricsScore<'a> {
-//     scores: Vec<Ap<'a>>,
-// }
+impl DetectionMetricsScore {
+    pub(crate) fn new(
+        results_map: &HashMap<String, Vec<PerceptionResult>>,
+        num_gt_map: &HashMap<String, usize>,
+        target_labels: &Vec<Label>,
+        matching_mode: &MatchingMode,
+        matching_thresholds: &Vec<f64>,
+    ) -> Self {
+        let mut scores = HashMap::new();
+        let mut ap = 0.0;
+        let mut aph = 0.0;
+        let mut n = 0.0;
+        for (target_label, threshold) in target_labels.iter().zip(matching_thresholds.iter()) {
+            let results = results_map[&target_label.to_string()];
+            let num_gt = num_gt_map[&target_label.to_string()];
+            ap += Ap::new(&results, &num_gt, target_label).calculate_ap(
+                TPMetricsAP,
+                matching_mode,
+                threshold,
+            );
 
-// impl<'a> DetectionMetricsScore<'a> {
-//     pub(crate) new()
-// }
+            aph += Ap::new(&results, &num_gt, target_label).calculate_ap(
+                TPMetricsAPH,
+                matching_mode,
+                threshold,
+            );
+
+            n += 1.0;
+        }
+
+        scores.insert("ap".to_string(), ap / n);
+        scores.insert("aph".to_string(), aph / n);
+
+        Self { scores: scores }
+    }
+}
 
 #[derive(Debug)]
 pub(super) struct Ap<'a> {
     results: &'a Vec<PerceptionResult>,
-    num_ground_truth: isize,
-    target_labels: &'a Vec<Label>,
+    num_ground_truth: &'a usize,
+    target_label: &'a Label,
 }
 
 impl<'a> Ap<'a> {
     pub(super) fn new(
         results: &'a Vec<PerceptionResult>,
-        num_ground_truth: isize,
-        target_labels: &'a Vec<Label>,
+        num_ground_truth: &usize,
+        target_label: &'a Label,
     ) -> Self {
         Self {
             results: results,
             num_ground_truth: num_ground_truth,
-            target_labels: target_labels,
+            target_label: target_label,
         }
     }
 
@@ -37,12 +68,12 @@ impl<'a> Ap<'a> {
         &self,
         tp_metrics: T,
         matching_mode: &MatchingMode,
-        thresholds: &Vec<f64>,
+        threshold: &f64,
     ) -> f64
     where
         T: TPMetrics,
     {
-        let (tp_list, _) = self.calculate_tp_fp(tp_metrics, matching_mode, thresholds);
+        let (tp_list, _) = self.calculate_tp_fp(tp_metrics, matching_mode, threshold);
         let (precision_list, recall_list) = self.calculate_precision_recall(&tp_list);
         let (max_precision_list, max_recall_list) =
             self.interpolate_precision_recall(precision_list, recall_list);
@@ -64,16 +95,23 @@ impl<'a> Ap<'a> {
         precision_list: Vec<f64>,
         recall_list: Vec<f64>,
     ) -> (Vec<f64>, Vec<f64>) {
-        if self.results.len() == 0 && self.num_ground_truth == 0 {
+        if self.results.len() == 0 && *self.num_ground_truth == 0 {
             (Vec::new(), Vec::new())
         } else {
-            // TODO
-            (Vec::new(), Vec::new())
+            let mut max_precision_list = vec![*precision_list.last().unwrap()];
+            let mut max_recall_list = vec![*recall_list.last().unwrap()];
+            for i in 0..recall_list.len() - 1 {
+                if max_precision_list.last().unwrap() < &precision_list[i] {
+                    max_precision_list.push(precision_list[i]);
+                    max_recall_list.push(recall_list[i]);
+                }
+            }
+            (max_precision_list, max_recall_list)
         }
     }
 
     fn calculate_precision_recall(&self, tp_list: &Vec<f64>) -> (Vec<f64>, Vec<f64>) {
-        if self.results.len() == 0 && self.num_ground_truth == 0 {
+        if self.results.len() == 0 && *self.num_ground_truth == 0 {
             (Vec::new(), Vec::new())
         } else {
             let num_results = self.results.len();
@@ -87,9 +125,9 @@ impl<'a> Ap<'a> {
                 .enumerate()
             {
                 let i_float = i as f64;
-                let num_gt_float = self.num_ground_truth as f64;
+                let num_gt_float = *self.num_ground_truth as f64;
                 *precision = tp / (i_float + 1.0);
-                if self.num_ground_truth > 0 {
+                if *self.num_ground_truth > 0 {
                     *recall = tp / num_gt_float;
                 }
             }
@@ -102,12 +140,12 @@ impl<'a> Ap<'a> {
         &self,
         tp_metrics: T,
         matching_mode: &MatchingMode,
-        thresholds: &Vec<f64>,
+        threshold: &f64,
     ) -> (Vec<f64>, Vec<f64>)
     where
         T: TPMetrics,
     {
-        if self.results.len() == 0 && self.num_ground_truth == 0 {
+        if self.results.len() == 0 && *self.num_ground_truth == 0 {
             (Vec::new(), Vec::new())
         } else {
             let num_results = self.results.len();
@@ -115,13 +153,7 @@ impl<'a> Ap<'a> {
             let mut fp_list = vec![0.0; num_results];
 
             for (i, result) in self.results.iter().enumerate() {
-                let threshold = get_label_threshold(
-                    &result.estimated_object.label,
-                    self.target_labels,
-                    thresholds,
-                )
-                .unwrap();
-                if result.is_result_correct(matching_mode, &threshold).unwrap() {
+                if result.is_result_correct(matching_mode, threshold).unwrap() {
                     tp_list[i] = tp_metrics.get_value(result);
                 } else {
                     fp_list[i] = 1.0;
--- src/metrics/score.rs
+++ src/metrics/score.rs
@@ -1,23 +1,73 @@
-use crate::result::PerceptionResult;
+use std::collections::HashMap;
 
-use super::config::MetricsConfig;
+use crate::{matching::MatchingMode, result::PerceptionResult};
+
+use super::{config::MetricsConfig, detection::DetectionMetricsScore};
 
 pub(crate) struct MetricsScore<'a> {
     pub(crate) config: &'a MetricsConfig<'a>,
+    pub(crate) scores: HashMap<String, HashMap<String, f64>>,
 }
 
 impl<'a> MetricsScore<'a> {
     pub(crate) fn new(config: &'a MetricsConfig) -> Self {
-        Self { config: config }
+        let scores: HashMap<String, HashMap<String, f64>> = HashMap::new();
+        Self {
+            config: config,
+            scores: scores,
+        }
     }
 
-    pub(crate) fn evaluate_detection(&self, results: &Vec<PerceptionResult>) {
-        for threshold in self.config.center_distance_thresholds {}
+    pub(crate) fn evaluate_detection(
+        &self,
+        results_map: &HashMap<String, Vec<PerceptionResult>>,
+        num_gt_map: &HashMap<String, usize>,
+    ) -> HashMap<String, HashMap<String, f64>> {
+        let mut scores = HashMap::new();
+        let center_distance_scores_map = DetectionMetricsScore::new(
+            results_map,
+            num_gt_map,
+            self.config.target_labels,
+            &MatchingMode::CenterDistance,
+            self.config.center_distance_thresholds,
+        )
+        .scores;
 
-        for threshold in self.config.plane_distance_thresholds {}
+        scores.insert("CenterDistance".to_string(), center_distance_scores_map);
 
-        for threshold in self.config.iou2d_thresholds {}
+        let plane_distance_scores_map = DetectionMetricsScore::new(
+            results_map,
+            num_gt_map,
+            self.config.target_labels,
+            &MatchingMode::PlaneDistance,
+            self.config.plane_distance_thresholds,
+        )
+        .scores;
 
-        for threshold in self.config.iou3d_thresholds {}
+        scores.insert("PlaneDistance".to_string(), plane_distance_scores_map);
+
+        let iou2d_scores_map = DetectionMetricsScore::new(
+            results_map,
+            num_gt_map,
+            self.config.target_labels,
+            &MatchingMode::Iou2d,
+            self.config.iou2d_thresholds,
+        )
+        .scores;
+
+        scores.insert("Iou2d".to_string(), iou2d_scores_map);
+
+        let iou3d_scores_map = DetectionMetricsScore::new(
+            results_map,
+            num_gt_map,
+            self.config.target_labels,
+            &MatchingMode::Iou3d,
+            self.config.iou3d_thresholds,
+        )
+        .scores;
+
+        scores.insert("Iou3d".to_string(), iou3d_scores_map);
+
+        scores
     }
 }
