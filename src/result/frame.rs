c4b9a9cfdb297b49859f73b1ffc1400b6fe9be62
ktro2828 <kotaro.uetake@tier4.jp>
2023-05-19 02:44:06 +0900

    feat: add frame result
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

---
+++ src/result/frame.rs
@@ -0,0 +1,32 @@
+use crate::{
+    dataset::FrameGroundTruth,
+    filter::{divide_objects_to_num, divide_results},
+    label::Label,
+    metrics::{config::MetricsConfig, score::MetricsScore},
+};
+
+use super::object::PerceptionResult;
+
+#[derive(Debug, Clone)]
+pub(crate) struct PerceptionFrameResult<'a> {
+    pub score: MetricsScore<'a>,
+}
+
+impl<'a> PerceptionFrameResult<'a> {
+    pub fn new(config: &'a MetricsConfig) -> Self {
+        Self {
+            score: MetricsScore::new(config),
+        }
+    }
+
+    pub fn evaluate(
+        &self,
+        results: &Vec<PerceptionResult>,
+        frame_ground_truth: &FrameGroundTruth,
+        target_labels: &Vec<Label>,
+    ) {
+        let results_map = divide_results(results, target_labels);
+        let num_gt_map = divide_objects_to_num(&frame_ground_truth.objects, target_labels);
+        self.score.evaluate_detection(&results_map, &num_gt_map);
+    }
+}
